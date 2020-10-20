c8731409013464bb5e75fe6fb911fa20d246a4d1
justinekimchi <justine.kim.manaloto@gmail.com>
2020-10-20 11:15:09 -0500

    successfully tested seed file with migrations

---
+++ db/app_cli.rb
@@ -0,0 +1,12 @@
+# require_relative './config/environment'
+# class AppCLI
+#     def welcome_message
+#         puts " "
+#         puts "Welcome to Wash On Wheels! How can we W.O.W. you today?"
+#         puts " "
+#     end
+#     def run
+#         welcome_message
+#     end
+# end
+# AppCLI.new.run
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,20 @@
+require 'pry'
+
+Customer.destroy_all
+Package.destroy_all
+Service.destroy_all
+Vehicle.destroy_all
 
 c1 = Customer.create(name: "Daphne Blake", address: "2222 Haunted House Lane", phone_number: 4078808608, email: "daphneb@gmail.com")
 c2 = Customer.create(name: "Fred Jones", address: "3333 Marigold Street", phone_number: 8325558878, email: "freddyj@hotmail.com")
 c3 = Customer.create(name: "Velma Dinkley", address: "7777 Austin Powers Drive", phone_number: 9873256721, email: "queenv@yahoo.com")
 c4 = Customer.create(name: "Shaggy Rogers", address: "5555 Reina Boulevard", phone_number: 6785554321, email: "shaggman@gmail.com")
 
-v1 = Vehicle.create(make: "Ford", model: "F150", customer_id: 1, vehicle_type: "Truck")
-v2 = Vehicle.create(make: "Ford", model: "Courier", customer_id: 1, vehicle_type: "Van")
-v3 = Vehicle.create(make: "Nissan", model: "Sentra", customer_id: 2, vehicle_type: "Sedan")
-v4 = Vehicle.create(make: "Chevrolet", model: "Corvette", customer_id: 3, vehicle_type: "Coupe")
-v5 = Vehicle.create(make: "Toyota", model: "Tundra", customer_id: 4, vehicle_type: "Truck")
+v1 = Vehicle.create(make: "Ford", model: "F150", customer_id: c1, vehicle_type: "Truck")
+v2 = Vehicle.create(make: "Ford", model: "Courier", customer_id: c1, vehicle_type: "Van")
+v3 = Vehicle.create(make: "Nissan", model: "Sentra", customer_id: c2, vehicle_type: "Sedan")
+v4 = Vehicle.create(make: "Chevrolet", model: "Corvette", customer_id: c3, vehicle_type: "Coupe")
+v5 = Vehicle.create(make: "Toyota", model: "Tundra", customer_id: c4, vehicle_type: "Truck")
 
 p1 = Package.create(name: "Exterior Auto Detailing" , features: "Hand washing and drying, waxing and buffing, rim cleaning and polishing, degreasing and debugging." , price: 100)
 p2 = Package.create(name: "Interior Auto Detailing", features: "Seat cleaing, interior surface cleaning, floor cleaning, and deodorizing", price: 100)
@@ -20,3 +26,6 @@ s3 = Service.create(customer_id: c1, vehicle_id: v2, package_id: p3)
 s4 = Service.create(customer_id: c2, vehicle_id: v3, package_id: p1)
 s5 = Service.create(customer_id: c3, vehicle_id: v4, package_id: p1)
 s6 = Service.create(customer_id: c4, vehicle_id: v5, package_id: p2)
+
+binding.pry
+0
